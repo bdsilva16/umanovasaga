6e0d59431d65a8821a95259395cfd033c15a5cd8
bdsilva16 <bd686610@gmail.com>
2023-05-31 21:38:08 -0300

    atividade de conta doda vez que aperta no botão

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import * as Font from "expo-font"
+
 
 export default function App() {
   const [contador, setContador] = useState(0);
@@ -8,17 +11,49 @@ export default function App() {
     setContador(contador + 1);
   };
 
+  const reiniciarContagem = () => {
+    setContador(0);
+  };
+
+  const [fonteCarregada, setFonteCarregada] = useState(false)
+
+
+  async function loadFonts(){
+    await Font.loadAsync({
+      Display_Regular: require('./assets/DM_Serif_Display/DMSerifDisplay-Regular.ttf'),
+      Display_Italic: require('./assets/DM_Serif_Display/DMSerifDisplay-Italic.ttf'),
+      Marker_Regular: require('./assets/Permanent_Marker/PermanentMarker-Regular.ttf'),
+      Start2P_Regular: require('./assets/Press_Start_2P (1)/PressStart2P-Regular.ttf')
+    });
+  }
+
+  useEffect(() =>{
+    loadFonts()
+   .then(() => {
+    setFonteCarregada(true);
+   })
+   .catch((error) => {
+    console.log('Ocorreu um erro',error);
+   })
+  },[])
+
   return (
     <View style={styles.container}>
+      
       <Text style={styles.text}>VOCÊ CLICOU</Text>
-      <Text style={{fontSize:200, marginBottom:250}}>{contador}</Text>
+      <Text style={{ fontSize: 200, marginBottom: 250 }}>{contador}</Text>
 
-      <TouchableHighlight
-        style={styles.button}
-        onPress={handlePress}
-      >
-        <Text >Clique aqui!!!</Text>
-      </TouchableHighlight>
+      <View style={styles.containerbutton}>
+        {fonteCarregada}
+        <TouchableHighlight style={styles.button} onPress={handlePress}>
+          <Text>Clique aqui!!!</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.resetButton} onPress={reiniciarContagem}>
+          <Text>Reiniciar</Text>
+        </TouchableHighlight>
+      </View>
+
     </View>
   );
 }
@@ -29,6 +64,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 
   button: {
@@ -39,17 +75,38 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     borderRadius: 100,
-    position: 'absolute',
-    bottom: 50,
     borderColor: 'red',
     borderWidth: 2,
+    
+
+  },
+
+  resetButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 6,
+    backgroundColor: '#fff',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    borderColor: 'blue',
+    borderWidth: 2,
+    margin:10
+
   },
 
   text: {
     fontSize: 50,
-    marginBottom:20,
-    
-  } 
+    marginBottom: 20,
+  },
 
-
+  containerbutton: {
+    flexDirection: 'row',
+    position: "absolute",
+    width: "100%",
+    height: 200,
+    bottom: 40,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
